d4ddd4812e8b96e6de6ab359dc5e702b2c9c8838
Eugene Kutas <evgenij.kutas@gmail.com>
2022-06-27 23:38:00 +0300

    Demo pages added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        This is our first site with <span class="tag is-large is-primary">Vue.js</span>,<span class="tag is-large is-success">Vite</span> and <span class="tag is-large is-info">Bulma</span>!
+      </p>
+      <p class="content">
+        Now please, <router-link class="is-underlined" to="/next-page">visit next page</router-link>...
+      </p>
+      <p>BTW... I'm the <b class="has-text-primary">HomeView</b> component. You can check me out in the <i class="has-text-grey-darker has-text-weight-bold">"src/views/HomeView.vue"</i> file</p>
+    </div>
+  </section>
 </template>
---
+++ src/views/NextPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <p class="title">
+        Hi again!
+      </p>
+      <p class="subtitle">
+        I'm "Next Page")
+      </p>
+      <p><button class="button is-success" @click="$router.push('/')">Go home</button> </p>
+    </div>
+  </section>
+
+</template>
